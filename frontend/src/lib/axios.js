d33dea34d309846246fb4d72463104d08d3ddcf8
DevanshBajpai09 <devanshbajpai09@gmail.com>
2025-08-02 23:35:03 +0530

    Update base URL in useAuthContext and axios instance to production endpoint

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5001/api', // Replace with your backend's base URL
+    baseURL: 'https://chat-app-1-backend.onrender.com/api', // Replace with your backend's base URL
     
     withCredentials: true, // To include cookies for authentication
 });
